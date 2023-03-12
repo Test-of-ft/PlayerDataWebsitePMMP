692479437f31c4cf019544a619b8d17d9171ee34
ItsToxic <95104544+ItsToxicGG@users.noreply.github.com>
2023-03-12 04:57:56 -0700

    Update testingsql.php

--- testing/testingsql.php
+++ testing/testingsql.php
@@ -1,1 +1,16 @@
+<?php
 
+$servername = "localhost";
+$username = "stats";
+$password = "";
+$dbname = "stats";
+
+// Create connection #1
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection #2
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+?>
